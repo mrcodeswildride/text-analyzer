04a6e0b780b625b392b658349da7a57c5324349c
David Keller <mrcodeswildride@gmail.com>
2020-06-16 14:47:12 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# text-analyzer
+
+Text analyzer web app written in HTML, CSS, and JavaScript.
+
+Use this app at [textanalyzer.mrcodeswildride.com](https://textanalyzer.mrcodeswildride.com/).
+Learn how to make this app at [mrcodeswildride.com](https://www.mrcodeswildride.com/).
---
+++ script.js
@@ -0,0 +1,40 @@
+let words = document.getElementById(`words`)
+let frequencies = document.getElementById(`frequencies`)
+
+words.addEventListener(`input`, countCharacters)
+words.focus()
+
+function countCharacters() {
+  frequencies.innerHTML = ``
+  let counts = {}
+  let total = 0
+
+  for (let char of words.value) {
+    if (char != ` `) {
+      if (counts[char] == null) {
+        counts[char] = 0
+      }
+
+      counts[char]++
+      total++
+    }
+  }
+
+  for (let char in counts) {
+    let percent = (counts[char] / total) * 100
+
+    let frequency = document.createElement(`div`)
+    frequency.classList.add(`frequency`)
+    frequencies.appendChild(frequency)
+
+    let characterDiv = document.createElement(`div`);
+    characterDiv.classList.add(`character`)
+    characterDiv.innerHTML = char
+    frequency.appendChild(characterDiv)
+
+    let percentDiv = document.createElement(`div`)
+    percentDiv.classList.add(`percent`)
+    percentDiv.innerHTML = `${percent.toFixed(2)}%`
+    frequency.appendChild(percentDiv)
+  }
+}
